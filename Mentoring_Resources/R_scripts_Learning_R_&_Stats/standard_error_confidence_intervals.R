683520b400a1372a17b3ca5349c45086b10d5bf7
Maggie Schedl <margaretschedl@gmail.com>
2022-05-23 11:43:07 -0500

    adding SE script

---
+++ Mentoring_Resources/R_scripts_Learning_R_&_Stats/standard_error_confidence_intervals.R
@@ -0,0 +1,70 @@
+# Learning how to calculate the Standard Error of the Mean and the "Quick" 95% Confidence Interval in R 
+
+
+# Read in the dataframes 
+egg_counts_main <- read.csv("innubila_egg_counts_main_sheet.csv") # read in main datasheet
+egg_counts_individual <- read.csv("innubila_egg_counts_individual_plate_counts.csv") # read in plate count datasheet
+
+# Calculate standard error of the mean of average_egg_count by fly age 
+
+# separate out counts into ages 2 and 3
+# use the subset function to take only the rows in the main sheet that are flies age 2 
+# Then turn the average egg count column into a list of counts 
+# the == double equal sign means that you are specifying the character in the fly_age column has to be two
+# and the two is in quotes because it is a character
+average_egg_count_2 <- subset(egg_counts_main, fly_age == "two") 
+average_egg_count_2 <- average_egg_count_2$average_egg_count
+# then make your own function that is the equation for standard error 
+# this is the standard deviation of your data, divided by the square root of the number of observations
+std <- function(x) sd(x)/sqrt(length(x)) # note that when you run this line of code once in this script you won't need to again
+# then use this function on the list you just created
+std(average_egg_count_2)
+# the output of this is 7.19
+# this means that the precision around the mean of average egg counts for age 2
+# (60.7 eggs, calculated in the previous script) is + or - 7.19 eggs 
+# or 60.7 ± 7.19 eggs 
+
+# this can then be done for flies ages 3 
+average_egg_count_3 <- subset(egg_counts_main, fly_age == "three")
+average_egg_count_3 <- average_egg_count_3$average_egg_count
+
+std(average_egg_count_3)
+# the precision around the mean of average egg counts for age 3 is 
+# 83.5 ± 7.40 eggs 
+
+#### 
+# Now on your own calculate the standard error of the mean of average egg counts for other categories:
+# for yeast or no yeast (either "Y" or "N" in yeast column)
+# for each plate type (either "MAS", "MNS", "AJS", or "MS" in the plate_type column)
+# remember to name lists differently so you don't overwrite things you've already made 
+
+
+# Calculating the "quick" version of 95% confidence interval of average_egg_counts by fly age 
+# we can follow the quick and dirty rule to get an approximate 95% confidence interval of the mean 
+# this is done by adding and subtracting 2 standard error of the means from the mean of your value of interest 
+
+# flies age 2 
+# create values for twice the SE of egg counts for age 2
+# and the average egg counts for age 2
+age_2_interval <- 7.19 * 2
+mean_age_2 <- 60.2
+
+# then you can do the math with the values
+mean_age_2 + age_2_interval
+mean_age_2 - age_2_interval
+# The approximate 95% confidence interval for egg counts for flies age 2 is 45.82 - 74.58 eggs 
+
+# flies age 3
+age_3_interval <- 7.4 * 2
+mean_age_3 <- 83.5 
+
+mean_age_3 + age_3_interval
+mean_age_3 - age_3_interval
+# the approximate 95% confidence interval for egg counts for flies age 3 is 68.7 - 98.3 eggs 
+
+### 
+# Now on your own calculate the approximate 95% confidence interval of average egg counts for other categories:
+# for yeast or no yeast
+# for each plate type 
+
+
