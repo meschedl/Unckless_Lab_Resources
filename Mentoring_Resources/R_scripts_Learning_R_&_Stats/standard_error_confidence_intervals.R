5f5ae18a1ff79db17c06f8b1200d4231ffe2636c
Maggie Schedl <margaretschedl@gmail.com>
2022-05-26 14:45:59 -0500

    updating scripts

--- Mentoring_Resources/R_scripts_Learning_R_&_Stats/standard_error_confidence_intervals.R
+++ Mentoring_Resources/R_scripts_Learning_R_&_Stats/standard_error_confidence_intervals.R
@@ -19,10 +19,10 @@ average_egg_count_2 <- average_egg_count_2$average_egg_count
 std <- function(x) sd(x)/sqrt(length(x)) # note that when you run this line of code once in this script you won't need to again
 # then use this function on the list you just created
 std(average_egg_count_2)
-# the output of this is 7.19
+# the output of this is 5.7
 # this means that the precision around the mean of average egg counts for age 2
-# (60.7 eggs, calculated in the previous script) is + or - 7.19 eggs 
-# or 60.7 ± 7.19 eggs 
+# (56.3 eggs, calculated in the previous script) is + or - 5.7 eggs 
+# or 56.3 ± 5.7 eggs 
 
 # this can then be done for flies ages 3 
 average_egg_count_3 <- subset(egg_counts_main, fly_age == "three")
@@ -30,7 +30,7 @@ average_egg_count_3 <- average_egg_count_3$average_egg_count
 
 std(average_egg_count_3)
 # the precision around the mean of average egg counts for age 3 is 
-# 83.5 ± 7.40 eggs 
+# 79.3 ± 6.7 eggs 
 
 #### 
 # Now on your own calculate the standard error of the mean of average egg counts for other categories:
@@ -46,21 +46,21 @@ std(average_egg_count_3)
 # flies age 2 
 # create values for twice the SE of egg counts for age 2
 # and the average egg counts for age 2
-age_2_interval <- 7.19 * 2
-mean_age_2 <- 60.2
+age_2_interval <- 5.7 * 2
+mean_age_2 <- 56.3
 
 # then you can do the math with the values
 mean_age_2 + age_2_interval
 mean_age_2 - age_2_interval
-# The approximate 95% confidence interval for egg counts for flies age 2 is 45.82 - 74.58 eggs 
+# The approximate 95% confidence interval for egg counts for flies age 2 is 44.9 - 67.7 eggs 
 
 # flies age 3
-age_3_interval <- 7.4 * 2
-mean_age_3 <- 83.5 
+age_3_interval <- 6.7 * 2
+mean_age_3 <- 79.3 
 
 mean_age_3 + age_3_interval
 mean_age_3 - age_3_interval
-# the approximate 95% confidence interval for egg counts for flies age 3 is 68.7 - 98.3 eggs 
+# the approximate 95% confidence interval for egg counts for flies age 3 is 65.9 - 92.7 eggs 
 
 ### 
 # Now on your own calculate the approximate 95% confidence interval of average egg counts for other categories:
